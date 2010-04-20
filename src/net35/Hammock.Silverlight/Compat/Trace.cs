b1fb7cd83dc9a96427fdf44553f234b080547429
Crenna <none@none>
2010-04-20 14:43:22 -0400

    Added enabled toggle to Trace class and removed holding on to the isolated storage stream

--- src/net35/Hammock.Silverlight/Compat/Trace.cs
+++ src/net35/Hammock.Silverlight/Compat/Trace.cs
@@ -11,7 +11,10 @@ namespace Hammock.Silverlight.Compat
         private static IsolatedStorageFileStream _fileStream;
         private static StreamWriter _streamWriter;
 
-        static Trace()
+        public static bool Enabled { get; set; }
+        
+
+        private static void ObtainWriter()
         {
             _file = IsolatedStorageFile.GetUserStoreForApplication();
             _fileStream = _file.OpenFile("Hammock.trace",
@@ -45,24 +48,52 @@ namespace Hammock.Silverlight.Compat
 
         public static void WriteLine(string message)
         {
+            if (!Enabled)
+            {
+                return;
+            }
+
+            ObtainWriter();
+
             _streamWriter.WriteLine(message);
         }
 
         public static void WriteLineIf(bool condition, string message)
         {
+            if (!Enabled)
+            {
+                return;
+            }
+
             if(condition)
             {
+                ObtainWriter();
+
                 _streamWriter.WriteLine(message);
             }
         }
 
         public static void WriteLine(string message, params object[] args)
         {
+            if (!Enabled)
+            {
+                return;
+            }
+
+            ObtainWriter();
+
             _streamWriter.WriteLine(message, args);
         }
 
         public static void WriteLine(StringBuilder sb)
         {
+            if (!Enabled)
+            {
+                return;
+            }
+
+            ObtainWriter();
+
             _streamWriter.WriteLine(sb.ToString());
         }
     }
