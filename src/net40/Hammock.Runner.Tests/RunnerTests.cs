104047b547e1ea2e2341a5b3edfa5616dad5ca7c
Crenna <none@none>
2011-02-28 00:25:16 -0500

    Started a runner project

---
+++ src/net40/Hammock.Runner.Tests/RunnerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace Hammock.Runner.Tests
+{
+    [TestFixture]
+    public class RunnerTests
+    {
+        private const string Location = "http://hammockrest.com.s3.amazonaws.com/tests/";
+
+        [Test]
+        public void Can_get_usage_when_no_parameters_passed()
+        {
+            var output = WithHammock("");
+            Assert.IsNotNullOrEmpty(output);
+            Assert.IsTrue(output.StartsWith("Hammock"));
+        }
+
+        [Test]
+        public void Can_get_url()
+        {
+            var output = WithHammock(string.Concat(Location, "hello.html"));
+            Assert.IsNotNullOrEmpty(output);
+            Console.WriteLine(output);
+        }
+
+        private static string WithHammock(string command)
+        {
+            var process = new Process
+                              {
+                                  StartInfo =
+                                      {
+                                          UseShellExecute = false,
+                                          RedirectStandardOutput = true,
+                                          FileName = "hammock.exe",
+                                          Arguments = command
+                                      }
+                              };
+
+            process.Start();
+            var output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return output;
+        }
+    }
+}
---
+++ src/net40/Hammock.Runner/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+
+namespace Hammock
+{
+    public static class Program
+    {
+        private static readonly string _version;
+        private static readonly RestClient _client;
+
+        static Program()
+        {
+            _version = Assembly.GetAssembly(typeof (Program)).GetName().Version.ToString();
+            _client = new RestClient();
+        }
+
+        public static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                PrintUsage();
+                return;
+            }
+
+            if(args.Length == 1)
+            {
+                Uri uri;
+                if(Uri.TryCreate(args[0], UriKind.Absolute, out uri))
+                {
+                    _client.Authority = uri.Scheme + "://" + uri.Authority;
+                    var request = new RestRequest { Path = uri.PathAndQuery };
+                    var response = _client.Request(request);
+                    response.Content.Out();
+                    return;
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            "Hammock".Out();
+        }
+
+        private static void Out(this string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
